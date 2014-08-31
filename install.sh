5ce5b6e62b21dfdf8d659a8c82a6675ccf934106
Ionică Bizău <bizauionica@yahoo.com>
2014-08-31 18:13:21 +0300

    Exit if theme exists

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Backupping the current theme ..."
-gconftool-2 --dump '/apps/gnome-terminal' > ~/.terminal-theme.bkp.xml
+BACKUP_THEME_PATH="~/.terminal-theme.bkp.xml";
+if [ -f = $BACKUP_THEME_PATH ]
+then
+    echo "File $BACKUP_THEME_PATH exists. Please delete it before installing this package."
+    exit 1;
+fi
+gconftool-2 --dump '/apps/gnome-terminal' > $BACKUP_THEME_PATH
 echo "Importing the new theme ..."
 gconftool-2 --load terminal-flat-theme.xml
 echo "Done. Have fun! :-)"
